69d485fcb6263e4fd78c3a74350db3658a165dae
Aadil-Shabir <aadil.shabir13@gmail.com>
2022-04-19 11:32:10 +0500

    changes

--- db/index.js
+++ db/index.js
@@ -1,6 +1,12 @@
 const { Pool} = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PG_USER,      //postgres user
+  host: process.env.PG_ENDPOINT,  //localhost (I also tried 127.0.0.1)
+  database: process.env.PG_DB,    //database name to connect to
+  password: process.env.PG_PASS,  //postgres user password
+  port: process.env.PG_PORT       //5432
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
