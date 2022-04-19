4028376493fb5d28a01a84cbbb101df6061e5978
Aadil-Shabir <aadil.shabir13@gmail.com>
2022-04-19 10:58:57 +0500

    Changes

--- db/index.js
+++ db/index.js
@@ -1,24 +1,4 @@
-const { Pool, Client } = require("pg");
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-client.connect();
-
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
+const { Pool} = require("pg");
 
 const pool = new Pool();
 module.exports = {
