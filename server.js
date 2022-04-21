d59fa18f2f2a6802db64fac6aab8eba6cf6c3a9d
Aadil-Shabir <aadil.shabir13@gmail.com>
2022-04-21 08:58:54 +0500

    changed db import

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const db = require("./db");
+const db = require("./db/index");
 
 const app = express();
 
